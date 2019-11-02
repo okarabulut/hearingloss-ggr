4128835ea619d0712bdb4629db77723537dc49f4
Chris <chomsey1@gmail.com>
2019-11-01 22:09:23 -0400

    initial project structure, ui, api, react-router, eslint

---
+++ ui/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+const App = () => {
+  return (
+    <Router>
+      <div className="App">
+          Hello World
+      </div>
+    </Router>
+  );
+}
+
+export default App;
